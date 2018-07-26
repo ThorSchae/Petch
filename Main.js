22d237bca71e2fa97ee3b2bd5c2190395e414a77
ThorSchae <thorben_sch@web.de>
2018-07-26 12:33:46 +0200

    farben ändern bei mausbewegen

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 var circleX = 0;
 var offset = 0;
+var coloroffset = 0;
 function setup() {
     createCanvas(600, 400);
 }
@@ -11,6 +12,19 @@ function draw() {
     for(var x = 0; x <= mouseX; x +=50){  
       fill(random(255),random(255),random(255));
       ellipse(x, 200,25,25);
+      
     }
 offset = offset +1;
+//colorText();
+}
+
+onmousemove = function colorText(){
+    //for (var x = 0; x <= mouseX; x +=50){
+       var x =  document.getElementById("test1") 
+       x.style.color = '#'+ coloroffset;
+    
+    coloroffset = ((1<<24)*Math.random()|0).toString(16);
+    document.getElementById("option1").innerHTML = coloroffset
+   // y.textContent
+    
 }

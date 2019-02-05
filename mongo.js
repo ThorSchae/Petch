3a5c2c1e50e5ac499bd7deca16492248d66eada8
ThorSchae <thorben_sch@web.de>
2019-02-05 18:26:41 +0100

    mongo init erstellt: init muss erzeugt werden

--- mongo.js
+++ mongo.js
@@ -3,6 +3,10 @@ var url = "mongodb://localhost:27017/mydb";
 
 MongoClient.connect(url, function(err, db) {
 if (err) throw err;
-console.log("Database created!");
+var dbo = db.db("mydb");
+dbo.createCollection("customers", function(err, res){
+if (err) throw err;
+console.log("Collection created");
 db.close();
+})
 });
